1e0dd3ea4c1083cc2e9e2352821fc2a5db57f8a7
Rafał Grodziński <rafal_mikolaj_grodzinski@apple.com>
2024-12-20 17:16:24 +0900

    Controls view

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -15,7 +15,6 @@ struct ContentView: View {
             GLViewWrapper(config: config)
                 .background(Color.blue)
             ControlsView(config: $config)
-                .background(Color.red)
         }
     }
 }
@@ -23,7 +22,62 @@ struct ContentView: View {
 private struct ControlsView: View {
     @Binding var config: GLView.Config
 
+    @State private var tx: Float = 0
+
     var body: some View {
-        EmptyView()
+        VStack(spacing: 8) {
+            //Translate
+            Slider(
+                value: Binding(
+                    get: { config.tx },
+                    set: { config = config.updated(tx: $0) }
+                ),
+                in: GLView.Config.tRange
+            ) { Text("TX") }
+
+            Slider(
+                value: Binding(
+                    get: { config.ty },
+                    set: { config = config.updated(ty: $0) }
+                ),
+                in: GLView.Config.tRange
+            ) { Text("TY") }
+
+            Slider(
+                value: Binding(
+                    get: { config.tz },
+                    set: { config = config.updated(tz: $0) }
+                ),
+                in: GLView.Config.tRange
+            ) { Text("TZ") }
+
+            // Rotate
+            Slider(
+                value: Binding(
+                    get: { config.rx },
+                    set: { config = config.updated(rx: $0) }
+                ),
+                in: GLView.Config.rRange
+            ) { Text("RX") }
+
+            Slider(
+                value: Binding(
+                    get: { config.ry },
+                    set: { config = config.updated(ry: $0) }
+                ),
+                in: GLView.Config.rRange
+            ) { Text("RY") }
+
+            Slider(
+                value: Binding(
+                    get: { config.rz },
+                    set: { config = config.updated(rz: $0) }
+                ),
+                in: GLView.Config.rRange
+            ) { Text("RZ") }
+        }
+        .padding(8)
+        .frame(width: 200)
+        .frame(maxHeight: .infinity, alignment: .top)
     }
 }
--- Sources/GLView.swift
+++ Sources/GLView.swift
@@ -11,6 +11,33 @@ import OpenGL.GL
 
 extension GLView {
     struct Config {
+        static let tRange: ClosedRange<Float> = -1...1
+        static let rRange: ClosedRange<Float> = -180...180
+
+        let tx: Float
+        let ty: Float
+        let tz: Float
+
+        let rx: Float
+        let ry: Float
+        let rz: Float
+
+        init(tx: Float? = nil, ty: Float? = nil, tz: Float? = nil,
+             rx: Float? = nil, ry: Float? = nil, rz: Float? = nil) {
+            self.tx = tx ?? 0
+            self.ty = ty ?? 0
+            self.tz = tz ?? 0
+
+            self.rx = rx ?? 0
+            self.ry = ry ?? 0
+            self.rz = rz ?? 0
+        }
+
+        func updated(tx: Float? = nil, ty: Float? = nil, tz: Float? = nil,
+                    rx: Float? = nil, ry: Float? = nil, rz: Float? = nil) -> Config {
+            Config(tx: tx ?? self.tx, ty: ty ?? self.ty, tz: tz ?? self.tz,
+                   rx: rx ?? self.rx, ry: ry ?? self.ry, rz: rz ?? self.rz)
+        }
     }
 }
 
@@ -28,5 +55,6 @@ struct GLViewWrapper: NSViewRepresentable {
 
 class GLView: NSOpenGLView {
     var config: Config = Config() { didSet {
+        debugPrint(config)
     } }
 }
